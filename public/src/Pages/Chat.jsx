650a2a40da46c7f0be810a7e1e543d1ba72ab69b
aditya <adityasuman990@gmail.com>
2024-12-03 22:39:50 +0530

    first commit

---
+++ public/src/Pages/Chat.jsx
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
+import Contacts from "../Components/Contacts";
+import { allUsersRoute, host } from "../utils/APIRoutes";
+import Welcome from "../Components/Welcome";
+import ChatContainer from "../Components/ChatContainer";
+import { io } from "socket.io-client";
+
+function Chat() {
+  const socket = useRef();
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    const loadUser = async () => {
+      if (!localStorage.getItem("chat-app-user")) {
+        navigate("/login");
+      } else {
+        const storedUser = JSON.parse(localStorage.getItem("chat-app-user"));
+        setCurrentUser(storedUser);
+        setIsLoaded(true);
+      }
+    };
+    loadUser();
+  }, [navigate]);
+
+  useEffect(() => {
+    if (currentUser) {
+      socket.current = io(host);
+      socket.current.emit("add-user", currentUser._id);
+    }
+  }, [currentUser]);
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          try {
+            const { data } = await axios.get(
+              `${allUsersRoute}/${currentUser._id}`
+            );
+            setContacts(data);
+          } catch (error) {
+            console.error("Error fetching contacts:", error);
+          }
+        } else {
+          navigate("/setAvatar");
+        }
+      }
+    };
+    fetchContacts();
+  }, [currentUser, navigate]);
+
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
+  return (
+    <Container>
+      <div className="container">
+        <Contacts
+          contacts={contacts}
+          currentUser={currentUser}
+          changeChat={handleChatChange}
+        />
+        {isLoaded && currentChat === "undefined" ? (
+          <Welcome currentUser={currentUser} />
+        ) : (
+          <ChatContainer
+            currentChat={currentChat}
+            currentUser={currentUser}
+            socket={socket}
+          />
+        )}
+      </div>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #131324;
+
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
+
+export default Chat;
---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const messageRoute = require("./routes/messages");
+const Url = "mongodb+srv://hunkyjunkymoose:aZdFnJaizGM8Hqmq@cluster0.wbl2z.mongodb.net/chat-app?retryWrites=true&w=majority&appName=Cluster0"
+const app = express();
+const socket = require("socket.io");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoute);
+
+mongoose
+  .connect(Url, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection successful");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const server = app.listen(5000, () => {
+  console.log(`Server is running on 5000`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  })
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.io);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("mag-recieved", data.messsage);
+    }
+  })
+})
